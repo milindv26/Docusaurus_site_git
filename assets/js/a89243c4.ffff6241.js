d8fee7abd83fbaad8af8aeb2dced96baf3ebd34f
Milind <78922964+milindv26@users.noreply.github.com>
2024-08-02 17:12:27 +0100

    Deploy website - based on 1a3a35a8bc3f9142dc7c6444b43fc714ac6ef249

--- assets/js/a89243c4.ffff6241.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[9981],{2951:e=>{e.exports=JSON.parse('{"permalink":"/Notebooks_Courses/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/d171fc2c.ecb17a46.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[2024],{2481:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/Notebooks_Courses/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
